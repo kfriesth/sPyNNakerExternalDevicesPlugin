452a735d0e6af9b30bf4c39fc61b5b66291d11b3
sergiodavies <daviess@cs.man.ac.uk>
2016-01-20 18:56:45 +0000

    Completed development of on-chip data spec executor. File front_end_common_partitionable_graph_application_data_loader is left for reference, but is not required for the project.
    Merge remote-tracking branch 'remotes/origin/master' into c_DataSpec
    
    Merge remote-tracking branch 'remotes/origin/master' into c_dataSpec
    
    Conflicts:
    	spinnaker_graph_front_end/spinnaker_graph_front_end.py
    
    Merge remote-tracking branch 'remotes/origin/master' into c_dataSpec
    
    Conflicts:
    	c_common/front_end_common_lib/src/simulation.c
    	c_common/models/reverse_iptag_multicast_source/reverse_iptag_multicast_source.c
    	spinn_front_end_common/abstract_models/abstract_data_specable_vertex.py
    	spinn_front_end_common/interface/interface_functions/front_end_common_reload_script_creator.py
    	spinn_front_end_common/utilities/constants.py
    	spinn_front_end_common/utilities/reload/reload.py
    	spinn_front_end_common/utilities/reload/reload_script.py
    
    Merge remote-tracking branch 'remotes/origin/master' into c_dataSpec
    
    Conflicts:
    	spynnaker/pyNN/spinnaker.py
    	spynnaker/spynnaker.cfg
    
    Merge remote-tracking branch 'remotes/origin/master' into c_dataspec
    
    Conflicts:
    	spinnman/transceiver.py

--- neural_modelling/src/robot_motor_control/robot_motor_control.c
+++ neural_modelling/src/robot_motor_control/robot_motor_control.c
@@ -211,6 +211,7 @@ void c_main(void) {
     uint32_t timer_period = 0;
     if (!initialize(&timer_period)) {
         log_error("Error in initialisation - exiting!");
+        rt_error(RTE_SWERR);
         return;
     }
 
