606998ebc6a1b590689c0dfd68211eb11a2830d1
alan-stokes <alan.barry.stokes@gmail.com>
2016-03-14 09:41:34 +0000

    changes to partitions instead of edges

--- spynnaker_external_devices_plugin/pyNN/utility_models/spike_injector.py
+++ spynnaker_external_devices_plugin/pyNN/utility_models/spike_injector.py
@@ -1,6 +1,6 @@
 from spinn_front_end_common.abstract_models.\
-    abstract_provides_outgoing_edge_constraints import \
-    AbstractProvidesOutgoingEdgeConstraints
+    abstract_provides_outgoing_partition_constraints import \
+    AbstractProvidesOutgoingPartitionConstraints
 from spinn_front_end_common.utility_models.reverse_ip_tag_multi_cast_source\
     import ReverseIpTagMultiCastSource
 
@@ -12,13 +12,14 @@ from spynnaker.pyNN.utilities.conf import config
 
 
 class SpikeInjector(ReverseIpTagMultiCastSource,
-                    AbstractProvidesOutgoingEdgeConstraints):
+                    AbstractProvidesOutgoingPartitionConstraints):
     """ An Injector of Spikes for PyNN populations.  This only allows the user\
         to specify the virtual_key of the population to identify the population
     """
 
     def __init__(
             self, n_neurons, machine_time_step, timescale_factor, label, port,
+
             virtual_key=None):
 
         ReverseIpTagMultiCastSource.__init__(
@@ -26,7 +27,8 @@ class SpikeInjector(ReverseIpTagMultiCastSource,
             timescale_factor=timescale_factor, label=label, receive_port=port,
             virtual_key=virtual_key)
 
-    def get_outgoing_edge_constraints(self, partitioned_edge, graph_mapper):
+    def get_outgoing_partition_constraints(
+            self, partitioned_edge, graph_mapper):
         constraints = ReverseIpTagMultiCastSource\
             .get_outgoing_edge_constraints(
                 self, partitioned_edge, graph_mapper)
