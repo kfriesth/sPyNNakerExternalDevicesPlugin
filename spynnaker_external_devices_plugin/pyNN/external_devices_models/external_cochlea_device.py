dd2d2a39d077064a417829dc7fc1bf010d87507e
alan-stokes <alan.barry.stokes@gmail.com>
2016-06-09 12:08:34 +0100

    fixed cochlea device to wrok in new format.

--- spynnaker_external_devices_plugin/pyNN/external_devices_models/external_cochlea_device.py
+++ spynnaker_external_devices_plugin/pyNN/external_devices_models/external_cochlea_device.py
@@ -4,16 +4,37 @@ from pacman.model.abstract_classes.abstract_virtual_vertex \
 
 class ExternalCochleaDevice(AbstractVirtualVertex):
 
-    def __init__(
-            self, n_neurons, spinnaker_link, machine_time_step,
-            timescale_factor, label=None):
-        AbstractVirtualVertex.__init__(
-            self, n_neurons, spinnaker_link, label=label,
-            max_atoms_per_core=n_neurons)
+    population_parameters = {
+        'machine_time_step', 'time_scale_factor', 'spinnaker_link'}
 
-    @property
-    def model_name(self):
-        return "ExternalCochleaDevice:{}".format(self.label)
+    model_name = "ExternalCochleaDevice"
+
+    @staticmethod
+    def default_parameters(_):
+        return {}
+
+    @staticmethod
+    def fixed_parameters(_):
+        return {}
+
+    @staticmethod
+    def state_variables():
+        return list()
+
+    @staticmethod
+    def is_array_parameters(_):
+        return {}
+
+    def __init__(self, bag_of_neurons, label):
+
+        # can assume spinnaker link is consistent between atoms, as its a
+        # population parameter
+        spinnaker_link = \
+            bag_of_neurons[0].get_population_parameter('spinnaker_link')
+
+        AbstractVirtualVertex.__init__(
+            self, len(bag_of_neurons), spinnaker_link, label=label,
+            max_atoms_per_core=len(bag_of_neurons))
 
     def is_virtual_vertex(self):
         return True
