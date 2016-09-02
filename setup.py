5f3fde388350486c4d2f062bfd16e2de43fe447f
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2016-09-02 09:25:30 +0100

    Updated version for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="sPyNNakerExternalDevicesPlugin",
-    version="2016.001",
+    version="3.0.0",
     description="Spinnaker External Devices Plugin",
     url="https://github.com/SpiNNakerManchester/SpyNNaker",
     packages=['spynnaker_external_devices_plugin',
@@ -12,5 +12,5 @@ setup(
               'spynnaker_external_devices_plugin.pyNN.model_binaries',
               'spynnaker_external_devices_plugin.pyNN.utility_models'],
     package_data={'spynnaker_external_devices_plugin.pyNN.model_binaries': ['*.aplx']},
-    install_requires=['sPyNNaker == 2016.001']
+    install_requires=['sPyNNaker >= 3.0.0, < 4.0.0']
 )
